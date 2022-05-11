76cac6b1b10f24c88a46aea9b8a9045b4406f1a5
Deepak Karki <88586113+DeepakKarki12@users.noreply.github.com>
2022-05-11 21:00:46 +0530

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,5 +1,9 @@
 import PyPDF2 as pd
 from gtts import gTTS
+import streamlit as st
+
+
+st.title("pdf reader")
 
 path = open('Cracking_the_Coding_Interview_6th_Editio.pdf', 'rb')   # opening the pdf
 
@@ -7,7 +11,7 @@ pdfreader = pd.PdfFileReader(path)  # reading the pdf
 
 paged = pdfreader.numPages  # paged object will contain the number of pages our pdf has
 
-page = pdfreader.getPage(25)  # giving the page number of pdf which we wanna our computer to read
+page = pdfreader.getPage(24)  # giving the page number of pdf which we wanna our computer to read
 
 p_text = page.extractText()  # extracting the text from pdf and from given page number
 
@@ -18,5 +22,19 @@ print(line)
 
 
 # making and saving audio file
-obj = gTTS(f'{line}')
-obj.save('pdf25.mp3')
+# obj = gTTS(f'{line}')
+# obj.save('pdf25.mp3')
+#
+
+
+
+def convert_to_audio(text):
+    audio =gTTS(text=text, lang='en', tld='com')
+    audio.save("textaud.mp3")
+
+
+convert_to_audio(line)
+
+audio_file = open("textaud.mp3", "rb")
+
+st.audio(audio_file.read())
